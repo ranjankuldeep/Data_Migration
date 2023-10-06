2fa80a1a6f20ee64a034311a2e8e48300d996a23
ranjankuldeep <ranjankuldeep19@gmail.com>
2023-10-06 12:36:09 +0530

    feat: Insert Sql statement generated

--- main.go
+++ main.go
@@ -1,11 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello World")
 }
+
+// unmarshal the json string following the below structuture
+type OplogEntry struct {
+	Op string                 `json:"op"`
+	NS string                 `json:"ns"`
+	O  map[string]interface{} `json:"o"`
+	//here interface{} is any type so o is type of kind of javascript object
+}
+
 func GenerateInsertSQL(oplog string) (string, error) {
+	var oplogObj OplogEntry
+	// []byte(var) converts the var string to array of byte
+	if err := json.Unmarshal([]byte(oplog), &oplogObj); err != nil {
+		return "", nil
+	}
+	columnNames := make([]string, 0, len(oplogObj.O))
+
+	columnValues := make([]string, 0, len(oplogObj.O))
+	for columnName := range oplogObj.O {
+		columnNames = append(columnNames, columnName)
+	}
+	sort.Strings(columnNames)
+
+	for _, columnName := range columnNames {
+		columnValues = append(columnValues, getColumnValues(oplogObj.O[columnName]))
+	}
+
+	switch oplogObj.Op {
+	case "i":
+		sql := fmt.Sprintf("INSERT INTO " + oplogObj.NS)
+		sql = fmt.Sprintf("%s (%s) VALUES (%s);", sql, strings.Join(columnNames, ", "), strings.Join(columnValues, ", "))
+		return sql, nil
+
+	}
 
 	return "", nil
+}
+func getColumnValues(value interface{}) string {
+	switch value.(type) {
+	case int, int8, int16, int32, int64, float64:
+		return fmt.Sprintf("%v", value)
+	case bool:
+		return fmt.Sprintf("%t", value)
+	default:
+		return fmt.Sprintf("'%v'", value)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -10,7 +10,24 @@ func TestGenerateInsertSQL(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{name: "Insert Operation", oplog: "", want: "", wantErr: false},
+		{name: "Empty Operation",
+			oplog:   "",
+			want:    "",
+			wantErr: true,
+		},
+		{name: "Insert Operation", oplog: `{
+			"op" : "i",
+			"ns" : "test.student",
+			"o" : {
+			  "_id" : "635b79e231d82a8ab1de863b",
+			  "name" : "Selena Miller",
+			  "roll_no" : 51,
+			  "is_graduated" : false,
+			  "date_of_birth" : "2000-01-30"
+			}
+		  }`,
+			want:    "INSERT INTO test.student (_id, date_of_birth, is_graduated, name, roll_no) VALUES ('635b79e231d82a8ab1de863b', '2000-01-30', false, 'Selena Miller', 51);",
+			wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
